3cbe674022f2ad755be81f9ee8b488f21352a7eb
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-02-21 20:33:47 +0100

    change testUser

--- users/userController.spec.js
+++ users/userController.spec.js
@@ -1,10 +1,6 @@
-// const request = require('supertest');
-// const db = require('../database/db-config');
-// const server = require('../api/server');
-
-// beforeAll(async () => {
-//   await db.raw('TRUNCATE users RESTART IDENTITY CASCADE');
-// });
+const request = require('supertest');
+const db = require('../database/db-config');
+const server = require('../api/server');
 
 // const testUser = {
 //   slack_id: 'slack_id',
@@ -13,71 +9,83 @@
 //   img_72: 'image_url',
 // };
 
-// describe('userRouter', () => {
-//   let cookie;
-//   describe('POST /users/', () => {
-//     test('returns a 201 response after adding new user', async () => {
-//       const response = await request(server)
-//         .post('/users/')
-//         .send(testUser)
-//         .expect(201);
-//       cookie = response.headers['set-cookie'];
-//     });
+const testUser = {
+  full_name: 'Test 2',
+  slack_id: 'slack_id_two',
+  username: 'second_name',
+  email_address: 'email_address_two',
+  profile_picture: 'testurltwo',
+};
 
-//     test('returns a 200 if user already exists', async () => {
-//       const response = await request(server)
-//         .post('/users/')
-//         .send(testUser)
-//         .expect(200);
-//       cookie = response.headers['set-cookie'];
-//     });
-//   });
-//   describe('GET /users/:id', () => {
-//     test('returns a 200 response', async () => {
-//       await request(server)
-//         .get('/users/1')
-//         .set('Cookie', cookie)
-//         .expect('Content-Type', /json/)
-//         .expect(200);
-//     });
-//     test('returns an error when user does not exist', async () => {
-//       await request(server)
-//         .get('/users/10')
-//         .set('Cookie', cookie)
-//         .expect(400)
-//         .expect({ error: 'User does not exist' });
-//     });
-//   });
-//   describe('PUT /users/:id', () => {
-//     test('returns an error when user does not exist', async () => {
-//       const response = await request(server)
-//         .patch('/users/6')
-//         .send({
-//           full_name: 'Test 2',
-//         })
-//         .set('Cookie', cookie)
-//         .expect('Content-Type', /json/);
-//       expect(response.status).toEqual(400);
-//     });
-//     test('updates name successfully', async () => {
-//       await request(server)
-//         .patch('/users/1')
-//         .send({
-//           full_name: 'Test 1',
-//         })
-//         .set('Cookie', cookie)
-//         .expect(200);
-//     });
-//   });
-//   describe('GET /users/:id/logout', () => {
-//     test('cookie has been cleared', async () => {
-//       await request(server)
-//         .get('/users/1/logout')
-//         .expect(204);
-//     });
-//   });
-// });
+beforeAll(async () => {
+  await db.raw('TRUNCATE users RESTART IDENTITY CASCADE');
+});
 
-// afterAll(async () => {
-//   await db.raw('TRUNCATE users RESTART IDENTITY CASCADE');
-// });
+describe('userRouter', () => {
+  let cookie;
+  describe('POST /users/', () => {
+    test('returns a 201 response after adding new user', async () => {
+      const response = await request(server)
+        .post('/users/')
+        .send(testUser)
+        .expect(201);
+      cookie = response.headers['set-cookie'];
+    });
+
+    test('returns a 200 if user already exists', async () => {
+      const response = await request(server)
+        .post('/users/')
+        .send(testUser)
+        .expect(200);
+      cookie = response.headers['set-cookie'];
+    });
+  });
+  describe('GET /users/:id', () => {
+    test('returns a 200 response', async () => {
+      await request(server)
+        .get('/users/1')
+        .set('Cookie', cookie)
+        .expect('Content-Type', /json/)
+        .expect(200);
+    });
+    test('returns an error when user does not exist', async () => {
+      await request(server)
+        .get('/users/10')
+        .set('Cookie', cookie)
+        .expect(400)
+        .expect({ error: 'User does not exist' });
+    });
+  });
+  describe('PUT /users/:id', () => {
+    test('returns an error when user does not exist', async () => {
+      const response = await request(server)
+        .patch('/users/6')
+        .send({
+          full_name: 'Test 2',
+        })
+        .set('Cookie', cookie)
+        .expect('Content-Type', /json/);
+      expect(response.status).toEqual(400);
+    });
+    test('updates name successfully', async () => {
+      await request(server)
+        .patch('/users/1')
+        .send({
+          full_name: 'Test 1',
+        })
+        .set('Cookie', cookie)
+        .expect(200);
+    });
+  });
+  describe('GET /users/:id/logout', () => {
+    test('cookie has been cleared', async () => {
+      await request(server)
+        .get('/users/1/logout')
+        .expect(204);
+    });
+  });
+});
+
+afterAll(async () => {
+  await db.raw('TRUNCATE users RESTART IDENTITY CASCADE');
+});
